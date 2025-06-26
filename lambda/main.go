9038e4977646c8621452898b018e599dd189d9dd
thiago valle <zthiagovalle@gmail.com>
2025-06-26 12:10:49 -0300

    chore: removed unsed code

--- lambda/main.go
+++ lambda/main.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"lambda-func/app"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
-
-type MyEvent struct {
-	Username string `json:"username"`
-}
-
-// Take in a payload and something with it
-func HandleRequest(event MyEvent) (string, error) {
-	if event.Username == "" {
-		return "", fmt.Errorf("username cannot be empty")
-	}
-
-	return fmt.Sprintf("Successfully called by - %s", event.Username), nil
-}
 
 func main() {
 	myApp := app.NewApp()
